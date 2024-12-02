b67237e3d4282cce2ca8a27c9cc6b920dee6755b
maryem-fatnassi <estmaryam377@gmail.com>
2024-12-03 00:43:55 +0100

    initializing the project

---
+++ Navbar.js
@@ -0,0 +1,35 @@
+// Navbar Container..
+  // show tutorials menu in large screen..
+  const arrowDown_icon = document.getElementById('arrowDown_icon');
+  const tutorials_select = document.getElementById('tutorials-menu');
+  function selectedIcon(){
+    tutorials_select.classList.toggle('active');
+    arrowDown_icon.classList.toggle('rotate');
+  }
+  // show tutorials menu in small screen..
+  const arrowDownIcon = document.getElementById('arrow-icon');
+  const tutorialsMenu = document.getElementById('collapse');
+  function selectedDropDownIcon(){
+    tutorialsMenu.classList.toggle('activeMenu');
+    arrowDownIcon.classList.toggle('rotateIcon');
+  }
+ // show sideBar menu in small screen..
+ const sideBar = document.getElementById('side-navbar');
+ function selectedBars(){
+    sideBar.classList.toggle('show-sidebar');
+  }
+// End Of Navbar..
+
+
+
+
+
+
+
+
+
+
+
+// Section Two :  Benefits of Learning HTML CSS & JavaScript..
+
+// End of section two
---
+++ startCoding.js
@@ -0,0 +1,11 @@
+// Section Two :
+  // AOS to Definition Section ..
+const htmlDefinition = document.getElementById('html-definition').setAttribute('data-aos','fade-right');
+const javascriptDefinition = document.getElementById('javascript-definition').setAttribute('data-aos','fade-left');
+const htmlRelationship = document.getElementById('html-relationship').setAttribute('data-aos','fade-left');
+const javascriptRelationship = document.getElementById('javascript-relationship').setAttribute('data-aos','fade-right');
+AOS.init(
+    {
+        duration:2500,
+    }
+);

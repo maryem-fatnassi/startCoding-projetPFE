38455d635633f133ab49e148c913aac047945b73
maryem-fatnassi <estmaryam377@gmail.com>
2024-12-03 23:59:53 +0100

    Edit the third and fourth sections

--- startCoding.js
+++ startCoding.js
@@ -4,6 +4,12 @@ const htmlDefinition = document.getElementById('html-definition').setAttribute('
 const javascriptDefinition = document.getElementById('javascript-definition').setAttribute('data-aos','fade-left');
 const htmlRelationship = document.getElementById('html-relationship').setAttribute('data-aos','fade-left');
 const javascriptRelationship = document.getElementById('javascript-relationship').setAttribute('data-aos','fade-right');
+
+// Section Four : 
+  // AOS to the section four (VsCode & NotePad++ Container)
+const vsCodeContainer = document.getElementById('vs-code_container').setAttribute('data-aos','fade-right');
+const notePadContainer = document.getElementById('notepad++_container').setAttribute('data-aos','fade-left');
+
 AOS.init(
     {
         duration:2500,
